fc7a80c7686df818e6c10cf4e5c68913ff9c1cd7
Tyler Johnson <tyler@csdconsultants.us>
2021-01-28 22:06:42 -0800

    Filling out methods for cookie sold each hour calculation, and render method. Troubleshooting li error in console

--- js/app.js
+++ js/app.js
@@ -9,16 +9,35 @@ let seattle = {
   minimumCustomerEachHour: 23,
   maximumCustomerEachHour: 65,
   averageCookiesSoldPerCustomer: 6.3,
-  cookiesSoldPerHourarray: [],
+  cookiesSoldPerHourArray: [],
   dailyStoreTotal: 0,
-  randomCustomerEachHour: function (){
+
+  getRandomCustomerEachHour: function (){
     return Math.floor(Math.random() * (this.maximumCustomerEachHour - this.minimumCustomerEachHour + 1) + this.minimumCustomerEachHour);
   },
 
   calcCookiesSoldEachHour: function () {
-    let randomCustomerForOneHour = this.randomCustomerEachHour();
-    console.log(randomCustomerForOneHour);
+    for (let i = 0; i < hours.length; i++){
+      let randomCustomerForOneHour = this.getRandomCustomerEachHour();
+      let hourlyTotal = Math.ceil(randomCustomerForOneHour * this.averageCookiesSoldPerCustomer);
+      this.cookiesSoldPerHourArray.push(hourlyTotal);
+      this.dailyStoreTotal += hourlyTotal;
+      console.log(randomCustomerForOneHour);
+    }
+  },
+  render: function() {
+    this.calcCookiesSoldEachHour();
+    for (let i = 0; i < hours.length; i++) {
+      let li = document.createElement('li');
+      li.textContent = `${hours[i]}: ${this.cookiesSoldPerHourArray[i]} cookies`;
+      seattle.appendChild(li);
+    }
+    let li = document.createElement('li');
+    li.textContent = `Total: ${this.dailyStoreTotal} cookies`;
+    seattle.appendChild(li);
   }
 };
 
 // site used - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
+seattle.render();
