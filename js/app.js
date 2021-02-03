544ab75f35f70e3fa7798a0c09c6605b0f14b3fa
Tyler Johnson <tyler@csdconsultants.us>
2021-02-02 22:49:07 -0800

    Finished initial code including render, instatiations, and calls. Getting NaN errors and need to troubleshoot further

--- js/app.js
+++ js/app.js
@@ -3,6 +3,11 @@
 let hours = ['6am', '7am', '8am', '9am','10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
 let allStores = [];
+const cookieTable = document.querySelector('table');
+let tbody = document.createElement('tbody');
+cookieTable.appendChild(tbody);
+
+
 
 //comment these out?
 // let seattleList = document.getElementById('seattle');
@@ -52,6 +57,31 @@ Store.prototype.render = function () {
   tr.appendChild(td);
   tbody.appendChild(tr);
 };
+
+function renderHeader () {
+  let header = document.createElement('th');
+  for (let i = 0; i < hours.length; i++){
+    let th = document.createElement('th');
+    th.textContent = hours[i];
+    header.appendChild(th);
+  }
+  let th = document.createElement('th');
+  th.textContent = 'Store Daily Total';
+  header.appendChild(th);
+}
+
+let seattleStore = new Store('Seattle', 23, 65, 6.3, []);
+let tokyoStore = new Store('tokyo', 3, 24, 1.2, []);
+let dubaiStore = new Store('dubai', 11, 38, 3.7, []);
+let parisStore = new Store('paris', 20, 38, 2.3, []);
+let limaStore = new Store('lima', 2, 16, 4.6, []);
+
+renderHeader();
+seattleStore.render();
+tokyoStore.render();
+dubaiStore.render();
+parisStore.render();
+limaStore.render();
 
 // let seattle = {
 //   name: 'Seattle',
